eed0b5f2cbea5c41b82228d33e920b15eb5a9887
Leonardo Vizagre <leonardo.vizagre@gmail.com>
2022-05-21 12:04:30 -0300

    Novas funcionalidades do módulo 3

---
+++ src/main/java/com/algaworks/algalog/domain/service/SolicitacaoEntregaService.java
@@ -0,0 +1,38 @@
+package com.algaworks.algalog.domain.service;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.model.Entrega;
+import com.algaworks.algalog.domain.model.StatusEntrega;
+import com.algaworks.algalog.domain.repository.EntregaRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class SolicitacaoEntregaService {
+
+	private CatalogoClienteService catalogoClienteService;
+	private EntregaRepository entregaRepository;
+	
+	@Transactional
+	public Entrega solicitar (Entrega entrega) {
+		
+		/*	aqui vão as regras de negócio do caso de uso Solicitacao de Entrega
+			por exemplo horário de entrega, entregador disponível, etc...
+		*/
+		
+		Cliente cliente =  catalogoClienteService.buscar(entrega.getCliente().getId());
+		
+		entrega.setCliente(cliente);
+		entrega.setStatus(StatusEntrega.PENDENTE);
+		entrega.setDataPedido(LocalDateTime.now());		
+		return entregaRepository.save(entrega);
+		
+	}
+	
+}
